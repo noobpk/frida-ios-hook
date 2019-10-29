4ecceec46398c5d04d713c32e91dd52cd78d3627
noobpk <leethanhphuc@gmail.com>
2019-10-29 10:05:26 +0700

    Update return_value.js

--- return_value.js
+++ return_value.js
@@ -8,10 +8,12 @@ Interceptor.attach(hook.implementation, {
     onLeave: function(retval) { console.log("[*] Class Name: " + className);
     console.log("[*] Method Name: " + funcName); 
     console.log("\t[-] Type of return value: " + typeof retval);
-    console.log("\t[-] Original Return Value: " + retval); } }); }
+    console.log("\t[-] Original Return Value: " + retval);
+    //modify original return value
     // newretval = ptr("0x0") 
     // retval.replace(newretval) 
     // console.log("\t[-] New Return Value: " + newretval) 
+	} }); } 
 catch(err) { console.log("[!] Exception2: " + err.message); } } 
 
 else { console.log("Objective-C Runtime is not available!"); }
