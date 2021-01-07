37ecfb625554cdcc78b19dc011dfe08b27d09779
noobpk <ltp.noobpk@gmail.com>
2021-01-07 17:08:42 +0700

    update script bypass jailbreak add script url request

--- method/bypass_jailbreak.js
+++ method/bypass_jailbreak.js
@@ -1,26 +1,222 @@
-function bypassJailbreakDetection() {
+function bypassJailbreakDetection(){
+
+    var paths = [
+        "/Applications/blackra1n.app",
+        "/Applications/Cydia.app",
+        "/Applications/FakeCarrier.app",
+        "/Applications/Icy.app",
+        "/Applications/IntelliScreen.app",
+        "/Applications/MxTube.app",
+        "/Applications/RockApp.app",
+        "/Applications/SBSetttings.app",
+        "/Applications/WinterBoard.app",
+        "/bin/bash",
+        "/bin/sh",
+        "/bin/su",
+        "/etc/apt",
+        "/etc/ssh/sshd_config",
+        "/Library/MobileSubstrate/DynamicLibraries/LiveClock.plist",
+        "/Library/MobileSubstrate/DynamicLibraries/Veency.plist",
+        "/Library/MobileSubstrate/MobileSubstrate.dylib",
+        "/pguntether",
+        "/private/var/lib/cydia",
+        "/private/var/mobile/Library/SBSettings/Themes",
+        "/private/var/stash",
+        "/private/var/tmp/cydia.log",
+        "/System/Library/LaunchDaemons/com.ikey.bbot.plist",
+        "/System/Library/LaunchDaemons/com.saurik.Cydia.Startup.plist",
+        "/usr/bin/cycript",
+        "/usr/bin/ssh",
+        "/usr/bin/sshd",
+        "/usr/libexec/sftp-server",
+        "/usr/libexec/ssh-keysign",
+        "/usr/sbin/frida-server",
+        "/usr/sbin/sshd",
+        "/var/cache/apt",
+        "/var/lib/cydia",
+        "/var/log/syslog",
+        "/var/mobile/Media/.evasi0n7_installed",
+        "/var/tmp/cydia.log",
+        "/etc/apt",
+        "/Library/MobileSubstrate/MobileSubstrate.dylib",
+        "/Applications/Cydia.app",
+        "/Applications/blackra1n.app",
+        "/Applications/FakeCarrier.app",
+        "/Applications/Icy.app",
+        "/Applications/IntelliScreen.app",
+        "/Applications/MxTube.app",
+        "/Applications/RockApp.app",
+        "/Applications/SBSetttings.app",
+        "/private/var/lib/apt/",
+        "/Applications/WinterBoard.app",
+        "/usr/sbin/sshd",
+        "/private/var/tmp/cydia.log",
+        "/usr/binsshd",
+        "/usr/libexec/sftp-server",
+        "/Systetem/Library/LaunchDaemons/com.ikey.bbot.plist",
+        "/System/Library/LaunchDaemons/com.saurik.Cy@dia.Startup.plist",
+        "/var/log/syslog",
+        "/bin/bash",
+        "/bin/sh",
+        "/etc/ssh/sshd_config",
+        "/usr/libexec/ssh-keysign",
+        "/Library/MobileSubstrate/DynamicLibraries/Veency.plist",
+        "/System/Library/LaunchDaemons/com.ikey.bbot.plist",
+        "/private/var/stash",
+        "/usr/bin/cycript",
+        "/usr/bin/ssh",
+        "/usr/bin/sshd",
+        "/var/cache/apt",
+        "/var/lib/cydia",
+        "/var/tmp/cydia.log",
+        "/Applications/SBSettings.app",
+        "/Library/MobileSubstrate/DynamicLibraries/LiveClock.plist",
+        "/System/Library/LaunchDaemons/com.saurik.Cydia.Startup.plist",
+        "/private/var/lib/apt",
+        "/private/var/lib/cydia",
+        "/private/var/mobile/Library/SBSettings/Themes",
+        "/var/lib/apt",
+        "/private/jailbreak.txt",
+        "/bin/su",
+        "/pguntether",
+        "/usr/sbin/frida-server",
+        "/private/Jailbreaktest.txt",
+        "/var/mobile/Media/.evasi0n7_installed"
+    ];
+
     try {
-        var className = "JailbreakDetection";
-        var funcName = "+ isJail";
-        var hook = eval('ObjC.classes.' + className + '["' + funcName + '"]');
-        Interceptor.attach(hook.implementation, {
+
+        var f = Module.findExportByName("libSystem.B.dylib", "stat64");
+        Interceptor.attach(f, {
+            onEnter: function(args) {
+                this.is_common_path = false;
+                var arg = Memory.readUtf8String(args[0]);
+                for (var path in paths) {
+                    if (arg.indexOf(paths[path]) > -1) {
+                        console.log("Hooking native function stat64: " + arg);
+                        this.is_common_path = true;
+                        //return -1;
+                    }
+                }
+            },
             onLeave: function(retval) {
-                console.log("[*] Class Name: " + className);
-                console.log("[*] Method Name: " + funcName);
-                console.log("\t[-] Type of return value: " + typeof retval);
-                console.log("\t[-] Original Return Value: " + retval);
-                retval.replace(0x0);
-                console.log("\t[-] Type of return value: " + typeof retval);
-                console.log("\t[-] Return Value: " + retval);
+                if (this.is_common_path) {
+                    console.log("stat64 Bypass!!!");
+                    retval.replace(-1);
+                }
             }
         });
+        var f = Module.findExportByName("libSystem.B.dylib", "stat");
+        Interceptor.attach(f, {
+            onEnter: function(args) {
+                this.is_common_path = false;
+                var arg = Memory.readUtf8String(args[0]);
+                for (var path in paths) {
+                    if (arg.indexOf(paths[path]) > -1) {
+                        console.log("Hooking native function stat: " + arg);
+                        this.is_common_path = true;
+                        return -1;
+                    }
+                }
+            },
+            onLeave: function(retval) {
+                if (this.is_common_path) {
+                    console.log("stat Bypass!!!");
+                    retval.replace(-1);
+                }
+            }
+        });
+    }
+    catch(e) {
+        var message = {
+            type: 'exception',
+            data: {
+                message: '[!] Jailbreak bypass script error: '
+            }
+        };
+        send(message);
+    }
+    try {
+        var resolver = new ApiResolver('objc');
 
-    } catch (err) {
-        console.log("[-] Error: " + err.message);
+        resolver.enumerateMatches('*[* *jail**]', {
+            onMatch: function(match) {
+                var ptr = match["address"];
+                Interceptor.attach(ptr, {
+                    onEnter: function() {},
+                    onLeave: function(retval) {
+                        retval.replace(0x0);
+                    }
+                });
+            },
+            onComplete: function() {}
+        });
+
+        resolver.enumerateMatches('*[* fileExistsAtPath*]', {
+            onMatch: function(match) {
+                var ptr = match["address"];
+                Interceptor.attach(ptr, {
+                    onEnter: function(args) {
+                        var path = ObjC.Object(args[2]).toString();
+                        this.jailbreakCall = false;
+                        for (var i = 0; i < paths.length; i++) {
+                            if (paths[i] == path) {
+                                this.jailbreakCall = true;
+                            }
+                        }
+                    },
+                    onLeave: function(retval) {
+                        if (this.jailbreakCall) {
+                            retval.replace(0x0);
+                        }
+                    }
+                });
+            },
+            onComplete: function() {}
+        });
+
+        resolver.enumerateMatches('*[* canOpenURL*]', {
+            onMatch: function(match) {
+                var ptr = match["address"];
+                Interceptor.attach(ptr, {
+                    onEnter: function(args) {
+                        var url = ObjC.Object(args[2]).toString();
+                        this.jailbreakCall = false;
+                        if (url.indexOf("cydia") >= 0) {
+                            this.jailbreakCall = true;
+                        }
+                    },
+                    onLeave: function(retval) {
+                        if (this.jailbreakCall) {
+                            retval.replace(0x0);
+                        }
+                    }
+                });
+            },
+            onComplete: function() {}
+        });
+
+        var response = {
+            type: 'sucess',
+            data: {
+                message: "[!] Jailbreak Bypass success"
+            }
+        };
+        send(response);
+    }
+    catch(e) {
+        var message = {
+            type: 'exception',
+            data: {
+                message: '[!] Jailbreak bypass script error: '
+            }
+        };
+        send(message);
     }
 }
 
 if (ObjC.available) {
+    console.log("[*] Started: Bypass Jailbreak Detection")
     bypassJailbreakDetection();
 } else {
     send("error: Objective-C Runtime is not available!");
---
+++ method/url_request.js
@@ -0,0 +1,156 @@
+
+var colors = {
+            "resetColor": "\x1b[0m",
+            "green": "\x1b[32m",
+            "yellow": "\x1b[33m",
+            "red": "\x1b[31m"
+    }
+
+if (ObjC.available) {
+    console.log(colors.green,"[*] Started: Listening For Requests...",colors.resetColor);
+
+    try {  
+    
+        var className = "NSURLSession"; 
+        var funcName = "- dataTaskWithRequest:completionHandler:"; 
+
+        var hook = eval('ObjC.classes.' + className + '["' + funcName + '"]');
+        var OGCompletionHandler_DTWRM = null;
+
+        Interceptor.attach(hook.implementation, {  
+            
+            onEnter: function(args) 
+            {
+                console.log("-".repeat(20));
+                console.log(colors.green,"[+] HTTPMethod: ",colors.resetColor + ObjC.Object(args[2]).HTTPMethod() );
+                console.log(colors.green,"[+] URL: ",colors.resetColor + ObjC.Object(args[2]).URL().absoluteString() )
+                
+                var httpbody_nsdata = ObjC.Object(args[2]).HTTPBody();
+                var httpbody_nsstring = ObjC.classes.NSString.alloc().initWithData_encoding_(httpbody_nsdata, 4);
+                
+                console.log (colors.green,"[+] HTTPBody (NSString): ",colors.resetColor + httpbody_nsstring);
+                if (httpbody_nsstring += null) {
+                    console.log(colors.green,"[+] HTTPBody (NSData): ",colors.resetColor + httpbody_nsstring);
+                    console.log("");
+                } else{
+                    console.log(colors.yellow,"[!] HTTPBody Empty",colors.resetColor);
+                }
+
+                var completionHandler = new ObjC.Block(args[3]);
+                    OGCompletionHandler_DTWRM = completionHandler.implementation;
+
+                    completionHandler.implementation = function(data_nsdata, response_nsurlresponse, error_nserror){
+                            //console.log(colors.green,"[+] Response Headers: ",colors.resetColor + ObjC.Object(response_nsurlresponse));
+                            // Convert NSData to NSString
+                            var data_nsstring = ObjC.classes.NSString.alloc().initWithData_encoding_(data_nsdata, 4);
+
+                            console.log(colors.green,"[+] Response Data: ",colors.resetColor + data_nsstring)
+
+                            // return original completion handler
+                            return OGCompletionHandler_DTWRM(data_nsdata, response_nsurlresponse, error_nserror);
+                    }
+                console.log("-".repeat(20));
+            }  
+        });
+         
+    } 
+    catch(error) 
+    {
+        console.log(colors.red,"[!] Exception: ",colors.resetColor + error.message); 
+    }
+    //NSURLRequest 
+    try {
+
+        var className = "NSURLRequest";
+        var funcName = "- initWithURL:";
+
+        var hook = eval('ObjC.classes.' + className + '["' + funcName + '"]');
+
+        Interceptor.attach(hook.implementation, {
+
+
+            onEnter: function(args) {
+                console.log(colors.green,"NSURLRequest with URL: ",colors.resetColor + ObjC.Object(args[2]));
+            },
+
+        });
+
+    } catch (error) {
+        console.log(colors.red,"[!] Exception: ",colors.resetColor + error.message);
+    }
+
+    try {
+
+        var className = "SRWebSocket";//"LGSRWebSocket";
+        var funcName = "- send:";
+
+        var hook = eval('ObjC.classes.' + className + '["' + funcName + '"]');
+
+
+        Interceptor.attach(hook.implementation, {
+
+
+            onEnter: function(args) {
+                var socketURL = ObjC.Object(args[0]).url().absoluteString().toString();
+                var data = ObjC.Object(args[2]);
+                
+                console.log('LGSRWebSocket (' + ObjC.Object(args[0]) + ') ---> ' + socketURL);
+                console.log('Data: ' + data);
+                
+                for (var i = 0; i < data.length(); i++) {
+                    console.log(data.characterAtIndex_(i).toString(16) + ' --> ' + data.characterAtIndex_(i).toString());
+                }
+            },
+
+        });
+
+    } catch (error) {
+        console.log(colors.red,"[!] Exception: ",colors.resetColor + error.message);
+    }
+
+    try {
+
+        var className = "SRWebSocket";//"LGSRWebSocket";
+        var funcName = "- _handleMessage:";
+
+        var hook = eval('ObjC.classes.' + className + '["' + funcName + '"]');
+
+        Interceptor.attach(hook.implementation, {
+
+
+            onEnter: function(args) {
+                console.log(colors.green,"LGSRWebSocket received: ",colors.resetColor + ObjC.Object(args[2]));
+            },
+
+        });
+
+    } catch (error) {
+        console.log(colors.red,"[!] Exception: ",colors.resetColor + error.message);
+    }
+    //Cordova
+    try {
+
+        var className = "CDVInvokedUrlCommand";
+        var funcName = "+ commandFromJson:";
+
+        var hook = eval('ObjC.classes.' + className + '["' + funcName + '"]');
+
+        Interceptor.attach(hook.implementation, {
+
+
+            onEnter: function(args) {
+                console.log(colors.green,"CDVInvokedUrlCommand with data: ",colors.resetColor + ObjC.Object(args[2]));
+            },
+
+        });
+
+    } catch (error) {
+        console.log(colors.red,"[!] Exception: ",colors.resetColor + error.message);
+    }
+}  
+
+else { 
+
+console.log(colors.red,"Objective-C Runtime is not available!",colors.resetColor); 
+
+}
