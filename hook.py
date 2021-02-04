e6cd5bee8545fec494f4ed2aed543634c6c6e79a
noobpk <ltp.noobpk@gmail.com>
2021-02-04 21:44:23 +0700

    add option dump decrypt aplication optimize core hook.py

--- hook.py
+++ hook.py
@@ -19,7 +19,7 @@ from paramiko import SSHClient
 from scp import SCPClient
 from tqdm import tqdm
 import traceback
-from module.listapp import *
+from lib.listapp import *
 from lib.checkversion import *
 from lib.log import *
 
@@ -94,10 +94,11 @@ def handle_del_log():
 def main():
     try:
         
-        usage = "[>] python3 %prog [options] arg\n\n\r[>] Example for spawn or attach app with -s(--script) options:\npython3 hook.py -p com.apple.AppStore / [-n App Store] -s trace_class.js\n\n\r[>] Example for spawn or attach app with -m(--method) options:\npython3 hook.py -p com.apple.AppStore / [-n App Store] -m app-static"
+        usage = "[>] python3 %prog [options] arg\n\n\r[>] Example for spawn or attach app with -s(--script) options:\npython3 hook.py -p com.apple.AppStore / [-n 'App Store'] -s trace_class.js\n\n\r[>] Example for spawn or attach app with -m(--method) options:\npython3 hook.py -p com.apple.AppStore / [-n 'App Store'] -m app-static\n\n\r[>] Example dump decrypt ipa with -d(--dump) and -o(--output) options:\npython3 hook.py -p com.apple.AppStore / [-n 'App Store'] -d -o App_dump_name"
         parser = optparse.OptionParser(usage,add_help_option=False)
         info = optparse.OptionGroup(parser,"Information")
         quick = optparse.OptionGroup(parser,"Quick Method")
+        dump = optparse.OptionGroup(parser,"Dump decrypt IPA")
 
         parser.add_option('-h', "--help", action="help", dest="help", help="Show basic help message and exit")
         #Using options -p(--package) for spawn application and load script
@@ -109,7 +110,7 @@ def main():
 
         parser.add_option("-s", "--script", dest="script",
                         help="Frida Script Hooking", metavar="SCIPRT.JS")
-        parser.add_option("-d", "--dump", action="store_true", help="Dump decrypt application.ipa", dest="dump")
+
         parser.add_option("-c", "--check-version", action="store_true", help="Check iOS hook for the newest version", dest="checkversion")
         parser.add_option("-u", "--update", action="store_true", help="Update iOS hook to the newest version", dest="update")
 
@@ -118,26 +119,35 @@ def main():
         #Some options to get info from device and applications
         info.add_option("--list-devices",
                         action="store_true", help="List All Devices", dest="listdevices")
+        #Listapp option using the code of the AloneMonkey's repo frida-ios-dump - Link: https://github.com/AloneMonkey/frida-ios-dump
         info.add_option("--list-apps",
                         action="store_true", help="List The Installed apps", dest="listapps")
         info.add_option("--list-appinfo",
                         action="store_true", help="List Info of Apps on Itunes", dest="listappinfo")
         info.add_option("--list-scripts",
                         action="store_true", help="List All Scripts", dest="listscripts")
+        #Dump decrypt IPA using the code of the AloneMonkey's repo frida-ios-dump - Link: https://github.com/AloneMonkey/frida-ios-dump
+        dump.add_option("-d", "--dump", action="store_true", help="Dump decrypt application.ipa", dest="dumpapp")
+        dump.add_option("-o", "--output", action="store" , dest="output_ipa", help="Specify name of the decrypted IPA", metavar="OUTPUT_IPA", type="string")
 
-
+        parser.add_option_group(dump)
         parser.add_option_group(info)
         parser.add_option_group(quick)
 
         options, args = parser.parse_args()
-
+        
         methods = [
             "method/ios_list_apps.js", #0
             "method/static_analysis.js", #1
             "method/bypass_ssl.js", #2
             "method/bypass_jailbreak.js", #3
             "method/intercept_url_request.js", #4
-            "method/intercept_crypto.js" #5
+            "method/intercept_crypto.js", #5
+            "method/dump.js" #6
+        ]
+
+        libs = [
+            "lib/dump.py" #0
         ]
 
         if options.listdevices:
@@ -185,6 +195,8 @@ def main():
             else:
                 logger.error('[?] Script not found!')
 
+        #Spawning application and load script with output
+        
         #Attaching script to application
         elif options.name and options.script:
             if os.path.isfile(options.script):
@@ -287,8 +299,15 @@ def main():
             sys.exit(0)
 
         #dump decrypt application
-        elif options.dump:
-            logger.warning('[!] The Method Is Updating!!')
+        elif options.package and options.dumpapp:
+            logger.info('[*] Dumping...')
+            lib = libs[0]
+            if options.output_ipa is None:
+                cmd = shlex.split("python3 " + lib + " " + options.package)
+            else:
+                cmd = shlex.split("python3 " + lib + " " + options.package + " -o " + options.output_ipa)
+            subprocess.call(cmd)
+            sys.exit(0)
 
         else:
             logger.warning("[!] Specify the options. use (-h) for more help!")
@@ -317,4 +336,3 @@ def main():
 if __name__ == '__main__':
     run()
 
-    
