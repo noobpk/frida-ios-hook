092c8a8aca99dc6b60ccb918afa097f50225c8fa
noobpk <ltp.noobpk@gmail.com>
2025-10-06 17:37:23 +0700

    refactor iproxy startup logic to accept custom device port and improve input validation

--- frida-ios-hook/core/utils/config.py
+++ frida-ios-hook/core/utils/config.py
@@ -160,32 +160,6 @@ class check():
                             break
                         elif not iproxy_device_port.isdigit():
                             logger.error("[x_x] Please enter valid port number.")
-                        iproxy_start = input('[?] Do you want start iproxy 2222 22 (yes/no): ')
-                        yes_choices = ['yes', 'y']
-                        no_choices = ['no', 'n']
-                        if iproxy_start.lower() in yes_choices:
-                            logger.info("[*] Start iproxy ")
-                            cmd = shlex.split("iproxy " + str(APP_SSH['port']) + " 22")
-                            subprocess.Popen(cmd)
-                            time.sleep(2)
-                            break
-                        elif iproxy_start.lower() in no_choices:
-                            iproxy_device_port = input('[?] Input your device port (default 22): ')
-                            if iproxy_device_port == '':
-                                iproxy_device_port = 22
-                                logger.info("[*] Start iproxy ")
-                                cmd = shlex.split("iproxy " + str(APP_SSH['port']) + " " + str(iproxy_device_port))
-                                subprocess.Popen(cmd)
-                                time.sleep(2)
-                                break
-                            else:
-                                logger.info("[*] Start iproxy ")
-                                cmd = shlex.split("iproxy " + str(APP_SSH['port']) + " " + str(iproxy_device_port))
-                                subprocess.Popen(cmd)
-                                time.sleep(2)
-                                break
-                            sys.exit(0)
-                            break
                         else:
                             logger.info("[*] Start iproxy: iproxy " + str(APP_SSH['port']) + " " + str(iproxy_device_port))
                             cmd = shlex.split("iproxy " + str(APP_SSH['port']) + " " + str(iproxy_device_port))
